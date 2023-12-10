1c7b47b22e25462de07e7aefb05ed08b72bd6c97
Анастасия Мытарева <nastya.mytareva.04@gmail.com>
2023-12-10 22:56:09 +0400

    coctail and heap sort

--- console/console_s.cc
+++ console/console_s.cc
@@ -5,9 +5,13 @@ using namespace std;
 using namespace sorting;
 
 int main() {
-	vector<int> mas{ 4,3,6,2,7,8,5,1,9 };
+	
+	vector<int> mas{4,3,6,2,7,8,5,1,9};
 	cout << mas << endl;
-	cout << sorting_inserts(mas);
+	//cout << insert_sort(mas);
+	//cout << coctail_sort(mas);
+	cout << heap_sort(mas);
 	cout << mas << endl;
+
 	return 0;
 }
--- include/Sorting.h
+++ include/Sorting.h
@@ -17,18 +17,80 @@ namespace sorting {
         return stream;
     }
 
-    // implementation of sorting by inserts
-    Stats sorting_inserts(vector<int>& mas) {
+    Stats insert_sort(vector<int>& mas) {
         Stats stats;
         if (mas.size() < 2)
             return stats;
-        for (int i = 1; i < mas.size(); i++)
-        {
+        for (int i = 1; i < mas.size(); i++) {
             stats.comparison_count++;
             for (int j = i; j > 0 && mas[j - 1] > mas[j]; j--) {
                 swap(mas[j - 1], mas[j]);
                 stats.copy_count += 2;
             }
+        }
+        return stats;
+    }
+
+    Stats coctail_sort(vector<int>& mas) {
+        Stats stats;
+        int left = 1, right = mas.size() - 1;
+        while (left <= right){
+            for (int i = right; i >= left; i--) {
+                stats.comparison_count++;
+                if (mas[i - 1] > mas[i]) {
+                    swap(mas[i - 1], mas[i]);
+                    stats.copy_count += 2;
+                }
+            }
+            left++;
+            for (int i = left; i <= right; i++) {
+                stats.comparison_count++;
+                if (mas[i - 1] > mas[i]) {
+                    swap(mas[i - 1], mas[i]);
+                    stats.copy_count += 2;
+                }
+            }
+            right--;
+        }
+        return stats;
+    }
+
+    void heapify(vector<int>& mas, int size, int i, Stats& stats) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        stats.comparison_count++;
+        if (left < size && mas[left] > mas[largest]) {
+            largest = left;
+            stats.copy_count++;
+        }
+
+        stats.comparison_count++;
+        if (right < size && mas[right] > mas[largest]) {
+            largest = right;
+            stats.copy_count++;
+        }
+
+        stats.comparison_count++;
+        if (largest != i) {
+            swap(mas[i], mas[largest]);
+            stats.copy_count++;
+            heapify(mas, size, largest, stats);
+        }
+    }
+
+    Stats heap_sort(vector<int>& mas) {
+        Stats stats;
+        int size = mas.size();
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(mas, size, i, stats);
+        }
+
+        for (int i = size - 1; i > 0; i--) {
+            swap(mas[0], mas[i]);
+            stats.copy_count += 2;
+            heapify(mas, i, 0, stats);
         }
         return stats;
     }
