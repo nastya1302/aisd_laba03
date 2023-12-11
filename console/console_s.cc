09609ee20e9f8572b23b0f6fdf7634260b40ecfb
Анастасия Мытарева <nastya.mytareva.04@gmail.com>
2023-12-11 22:09:56 +0400

    корректная работа сортировки вставками, создание сортированного, обратно сортированного и рандомного массивов, вычисление средних значений для 100 рандомных массивов, оператор += для stats

--- console/console_s.cc
+++ console/console_s.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <Sorting.h>
 
 using namespace std;
@@ -6,12 +7,24 @@ using namespace sorting;
 
 int main() {
 	
-	vector<int> mas{4,3,6,2,7,8,5,1,9};
-	cout << mas << endl;
+	//vector<int> mas{4,3,6,2,7,8,5,1,9};
+	//cout << mas << endl;
 	//cout << insert_sort(mas);
 	//cout << coctail_sort(mas);
-	cout << heap_sort(mas);
-	cout << mas << endl;
+	//cout << heap_sort(mas);
+	//cout << mas << endl;
+
+	vector<size_t> size{1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,25000,50000,100000};
+	/*for (size_t i = 0; i < size.size(); ++i) {
+		Stats stats = average_stats(size[i], coctail_sort);
+		cout << stats;
+	}*/
+
+	for (size_t i = 0; i < size.size(); ++i) {
+		vector<int> mas = back_sorted_mas(size[i]);
+		Stats stats = insert_sort(mas);
+		cout << stats;
+	}
 
 	return 0;
 }
--- include/Sorting.h
+++ include/Sorting.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
@@ -9,6 +10,12 @@ namespace sorting {
     struct Stats {
         size_t comparison_count = 0;
         size_t copy_count = 0;
+
+        Stats& operator+=(const Stats& stats) {
+            comparison_count += stats.comparison_count;
+            copy_count += stats.copy_count;
+            return *this;
+        }
     };
 
     ostream& operator<<(std::ostream& stream, const Stats& stats) {
@@ -18,14 +25,14 @@ namespace sorting {
     }
 
     Stats insert_sort(vector<int>& mas) {
-        Stats stats;
-        if (mas.size() < 2)
-            return stats;
+        Stats stats; 
         for (int i = 1; i < mas.size(); i++) {
-            stats.comparison_count++;
-            for (int j = i; j > 0 && mas[j - 1] > mas[j]; j--) {
-                swap(mas[j - 1], mas[j]);
-                stats.copy_count += 2;
+            for (int j = i; j > 0; j--) {
+                ++stats.comparison_count;
+                if (mas[j - 1] >= mas[j]) {
+                    swap(mas[j - 1], mas[j]);
+                    stats.copy_count += 2;
+                }
             }
         }
         return stats;
@@ -36,7 +43,7 @@ namespace sorting {
         int left = 1, right = mas.size() - 1;
         while (left <= right){
             for (int i = right; i >= left; i--) {
-                stats.comparison_count++;
+                stats.comparison_count += 1;
                 if (mas[i - 1] > mas[i]) {
                     swap(mas[i - 1], mas[i]);
                     stats.copy_count += 2;
@@ -44,7 +51,7 @@ namespace sorting {
             }
             left++;
             for (int i = left; i <= right; i++) {
-                stats.comparison_count++;
+                stats.comparison_count += 1;
                 if (mas[i - 1] > mas[i]) {
                     swap(mas[i - 1], mas[i]);
                     stats.copy_count += 2;
@@ -60,19 +67,19 @@ namespace sorting {
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
-        stats.comparison_count++;
+        stats.comparison_count += 1;
         if (left < size && mas[left] > mas[largest]) {
             largest = left;
             stats.copy_count++;
         }
 
-        stats.comparison_count++;
+        stats.comparison_count += 1;
         if (right < size && mas[right] > mas[largest]) {
             largest = right;
             stats.copy_count++;
         }
 
-        stats.comparison_count++;
+        stats.comparison_count += 1;
         if (largest != i) {
             swap(mas[i], mas[largest]);
             stats.copy_count++;
@@ -93,6 +100,49 @@ namespace sorting {
             heapify(mas, i, 0, stats);
         }
         return stats;
+    }
+
+    int random_value(int from, int to) {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<> segment(from, to);
+        return segment(gen);
+    }
+
+    vector<int> random_mas(size_t size) {
+        vector<int> mas;
+        int min = 1, max = 100000;
+        for (int i = 0; i < size; ++i)
+            mas.push_back(random_value(min, max));
+        return mas;
+    }
+
+
+    Stats average_stats(int size, Stats(*sorted_func)(vector<int>& arr)) {
+        int count = 100;
+        Stats stats;
+        for (int i = 0; i < 100; i++) {
+            vector<int> mas = random_mas(size);
+            stats += sorted_func(mas);
+        }
+        stats.comparison_count /= count;
+        stats.copy_count /= count;
+        return stats;
+    }
+
+    
+    vector<int> sorted_mas(size_t size) {
+        vector<int> mas;
+        for (int i = 0; i < size; i++)
+            mas.push_back(i);
+        return mas;
+    }
+
+    vector<int> back_sorted_mas(size_t size) {
+        vector<int> mas;
+        for (int i = size; i > 0; i--)
+            mas.push_back(i);
+        return mas;
     }
 
     ostream& operator<<(std::ostream& stream, const vector<int>& mas) {
